c6d2e16ec1f45038032229e63d65b88d2ed8aef9
ragulhpSPK <ragulhp27@gmail.com>
2023-05-15 16:40:13 +0530

    time

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     MONGODB_URL: process.env.MONGODB_URL,
+    SECRET_KEY: process.env.SECRET_KEY,
+    PLAIN_TEXT: process.env.PLAIN_TEXT,
   },
 };
 
---
+++ pages/my-endpoint.js
@@ -0,0 +1,6 @@
+export const config = {
+  api: {
+    // Increase the timeout to 30 seconds
+    timeout: 30,
+  },
+};
