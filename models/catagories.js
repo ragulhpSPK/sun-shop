bf81a97fe61f5ddcc574a244690fad11a6eb697e
ragulhpSPK <ragulhp27@gmail.com>
2023-05-15 11:04:39 +0530

    erjh

--- models/catagories.js
+++ models/catagories.js
@@ -4,11 +4,9 @@ const catSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
     },
     image: {
       type: String,
-      required: true,
     },
   },
   {
--- pages/api/banner/index.js
+++ pages/api/banner/index.js
@@ -2,48 +2,44 @@ import dbconnect from "@/connection/conn";
 import Banner from "../../../models/banner";
 
 export default async function bannerController(req, res) {
-  try {
-    dbconnect();
-    switch (req.method) {
-      case "GET":
-        {
-          try {
-            const banner = await Banner.find();
-            return res.status(200).send({ data: banner });
-          } catch (err) {
-            return res.status(500).send({ message: "failed" });
-          }
+  dbconnect();
+  switch (req.method) {
+    case "GET":
+      {
+        try {
+          const banner = await Banner.find();
+          return res.status(200).send({ data: banner });
+        } catch (err) {
+          return res.status(500).send({ message: "failed" });
         }
-        break;
+      }
+      break;
 
-      case "POST":
-        {
-          try {
-            const banner = await new Banner({ ...req.body.data });
-            const result = banner.save();
-            return res.status(200).send({ data: result });
-          } catch (err) {
-            return res.status(500).send({ message: "failed" });
-          }
+    case "POST":
+      {
+        try {
+          const banner = await new Banner({ ...req.body.data });
+          const result = banner.save();
+          return res.status(200).send({ data: result });
+        } catch (err) {
+          return res.status(500).send({ message: "failed" });
         }
-        break;
+      }
+      break;
 
-      case "PUT":
-        {
-          try {
-            const banner = await Banner.findByIdAndUpdate(
-              { _id: req.body.id },
-              req.body.data
-            );
+    case "PUT":
+      {
+        try {
+          const banner = await Banner.findByIdAndUpdate(
+            { _id: req.body.id },
+            req.body.data
+          );
 
-            return res.status(200).send({ data: banner });
-          } catch (err) {
-            return res.status(500).send({ message: "failed" });
-          }
+          return res.status(200).send({ data: banner });
+        } catch (err) {
+          return res.status(500).send({ message: "failed" });
         }
-        break;
-    }
-  } catch (err) {
-    return res.status(500).send({ message: err.message });
+      }
+      break;
   }
 }
