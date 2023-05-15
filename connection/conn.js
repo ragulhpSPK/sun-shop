7b5d01b5cd65f12ce3da21441476c8aa8bce2a00
ragulhpSPK <ragulhp27@gmail.com>
2023-05-15 16:10:06 +0530

    loadsh

--- connection/conn.js
+++ connection/conn.js
@@ -3,7 +3,9 @@ const MONGODB_URL = process.env.MONGODB_URL;
 
 export default async function dbconnect() {
   try {
-    await mongoose.connect(MONGODB_URL);
+    await mongoose.connect(
+      "mongodb+srv://ragulhp:ragulhp2704@cluster0.h4z31tf.mongodb.net/sun?retryWrites=true&w=majority"
+    );
   } catch (err) {
     return err;
   }
