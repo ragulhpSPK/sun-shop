a3dc9d54dcced68d212b4f42e6a953caad6e38f0
ragulhpSPK <ragulhp27@gmail.com>
2023-05-16 09:56:48 +0530

    gs

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     SECRET_KEY: process.env.SECRET_KEY,
     PLAIN_TEXT: process.env.PLAIN_TEXT,
   },
+  serverRuntimeConfig: {
+    setTimeout: 30,
+  },
 };
 
 module.exports = nextConfig;
