875aec9670c4933451ab95fa1ba553d106caa281
ragulhpSPK <ragulhp27@gmail.com>
2023-05-24 09:44:36 +0530

    dhf

--- connection/conn.js
+++ connection/conn.js
@@ -1,10 +1,23 @@
-import mongoose from "mongoose";
+import * as mongoose from "mongoose";
 const MONGODB_URL = process.env.MONGODB_URL;
 
-export default async function dbconnect() {
-  try {
-    await mongoose.connect(MONGODB_URL);
-  } catch (err) {
-    console.log(err, "iygtkiuyt");
+// export default async function dbconnect() {
+//   try {
+//     await mongoose.connect(MONGODB_URL);
+//   } catch (err) {
+//     console.log(err, "iygtkiuyt");
+//   }
+// }
+
+const connection = {};
+
+async function dbconnect() {
+  if (connection.isConnected) {
+    return;
   }
+
+  const db = await mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
+  connection.isConnected = db.connections[0].readyState;
 }
+
+export default dbconnect;
